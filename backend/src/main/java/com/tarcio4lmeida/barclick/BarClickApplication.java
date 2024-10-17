5b08dafeba6441579a8f98f2192353b86a3055c6
tarcio almeida <tarcioalmeidalima@gmail.com>
2024-10-17 12:50:54 -0300

    Setup do projeto backend

---
+++ backend/src/main/java/com/tarcio4lmeida/barclick/BarClickApplication.java
@@ -0,0 +1,13 @@
+package com.tarcio4lmeida.barclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarClickApplication.class, args);
+	}
+
+}
