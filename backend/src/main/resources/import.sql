2c04e2db47855743ff65808fe02233a1209839b9
tarcio almeida <tarcioalmeidalima@gmail.com>
2024-10-22 17:43:37 -0300

    Colocando imagens para a aplicaçao

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -24,50 +24,50 @@ INSERT INTO tb_categoria (nome, created_At) VALUES ('Sobremesas', NOW());
 INSERT INTO tb_categoria (nome, created_At) VALUES ('Pratos principais', NOW());
 INSERT INTO tb_categoria (nome, created_At) VALUES ('Bebidas', NOW());
 --
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Fritas', 25.90, NOW(), 'Porção de batata palito frita, bem sequinha e crocante.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Dadinho de tapioca', 29.90, NOW(), 'Servido com geleia de pimenta especial, feita aqui,', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Torresmo', 14.90, NOW(), 'Individual, pra comer com a feijoada (ou não)!', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Saladinha', 14.90, NOW(), 'Porção individual de salada , com folhas frescas e tomate-cereja.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Fritas', 25.90, NOW(), 'Porção de batata palito frita, bem sequinha e crocante.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/1.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Dadinho de tapioca', 29.90, NOW(), 'Servido com geleia de pimenta especial, feita aqui,', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/2.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Torresmo', 14.90, NOW(), 'Individual, pra comer com a feijoada (ou não)!', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/3.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Saladinha', 14.90, NOW(), 'Porção individual de salada , com folhas frescas e tomate-cereja.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/4.jpg');
 --
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (1, 1);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (2, 1);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (3, 1);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (4, 1);
 --
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Brigadeiro de colher', 19.90, NOW(), 'Porção de batata palito frita, bem sequinha e crocante.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Merengue', 22.90, NOW(), 'Morango, suspiros e chantilly servidos em taça.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Pudim', 15.90, NOW(), 'Sem furinhos, tá? Também feito na casa.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Frutas da estação', 9.90, NOW(), 'Fatiadas, sem caroço e casca, servidas num pratinho.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Brigadeiro de colher', 19.90, NOW(), 'Porção de batata palito frita, bem sequinha e crocante.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/5.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Merengue', 22.90, NOW(), 'Morango, suspiros e chantilly servidos em taça.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/6.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Pudim', 15.90, NOW(), 'Sem furinhos, tá? Também feito na casa.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/7.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Frutas da estação', 9.90, NOW(), 'Fatiadas, sem caroço e casca, servidas num pratinho.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/8.jpg');
 --
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (5, 2);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (6, 2);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (7, 2);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (8, 2);
 --
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Bife à cavalo', 28.90, NOW(), 'Filé mignon grelhado, arroz, feijão, farofa e ovo.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Frango grelhado', 38.90, NOW(), 'Frango grelhado, arroz, feijão, farofa e salada.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Peixe do dia com legumes', 55.90, NOW(), 'Posta de peixe do dia, arroz e legumes no vapor', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Omelete', 29.90, NOW(), 'Omelete caprichado. arroz, feijão e farofa.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Bife à cavalo', 28.90, NOW(), 'Filé mignon grelhado, arroz, feijão, farofa e ovo.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/9.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Frango grelhado', 38.90, NOW(), 'Frango grelhado, arroz, feijão, farofa e salada.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/10.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Peixe do dia com legumes', 55.90, NOW(), 'Posta de peixe do dia, arroz e legumes no vapor', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/11.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Omelete', 29.90, NOW(), 'Omelete caprichado. arroz, feijão e farofa.', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/12.jpg');
 --
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (9, 3);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (10, 3);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (11, 3);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (12, 3);
 --
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Refrigerante', 5.90, NOW(), 'Lata 350ml', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Água', 3.90, NOW(), 'Garrafa 500ml com ou sem gás', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Cerveja', 9.90, NOW(), 'Garrafa 300ml', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Vinho da Casa', 23.90, NOW(), 'Taça 150ml, tinto ou branco', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Refrigerante', 5.90, NOW(), 'Lata 350ml', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/13.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Água', 3.90, NOW(), 'Garrafa 500ml com ou sem gás', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/14.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Cerveja', 9.90, NOW(), 'Garrafa 300ml', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/15.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Vinho da Casa', 23.90, NOW(), 'Taça 150ml, tinto ou branco', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/16.jpg');
 --
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (13, 4);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (14, 4);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (15, 4);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (16, 4);
 --
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Gin Tônica', 31.90, NOW(), 'Gin nacional, água tônica e especiarias', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Caipirinha', 14.00, NOW(), 'Copo 300ml', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/8-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Whisky', 18.00, NOW(), 'Dose 50ml', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/9-big.jpg');
-INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Cerveja Artesanal', 12.90, NOW(), 'Garrafa 355ml', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/6-big.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Gin Tônica', 31.90, NOW(), 'Gin nacional, água tônica e especiarias', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/17.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Caipirinha', 14.00, NOW(), 'Copo 300ml', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/18.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Whisky', 18.00, NOW(), 'Dose 50ml', 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/19.jpg');
+INSERT INTO tb_produto (nome, price, date, descricao, img_url) VALUES ('Cerveja Artesanal', 12.90, NOW(), 'https://raw.githubusercontent.com/tarcio4lmeida/barclick/refs/heads/main/imagens/20.jpg');
 --
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (17, 4);
 INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (18, 4);
